7a7464680dac2530880e9897970dcccaa9463a44
Arthur Fudali <arthurfudali5@gmail.com>
2024-09-19 16:52:42 -0300

    finalizacao do back end da parte pedidos

--- controllers/PedidosController.js
+++ controllers/PedidosController.js
@@ -1,26 +1,50 @@
-import express from 'express'
-const router = express.Router()
-router.get("/pedidos", (req,res) =>{
-    const pedidos = [
-        {
-          numeroPedido: 1,
-          valor: 2000,
-        },
-        {
-          numeroPedido: 2,
-          valor: 1500,
-        },
-        {
-          numeroPedido: 3,
-          valor: 3000,
-        },
-        {
-          numeroPedido: 4,
-          valor: 10000,
-        },
-      ];
-      res.render("pedidos", {
-        pedidos:pedidos
-      })
-})
+import express from "express";
+const router = express.Router();
+router.get("/pedidos", (req, res) => {
+  const pedidos = [
+    {
+      numeroPedido: 1,
+      valor: 2000,
+    },
+    {
+      numeroPedido: 2,
+      valor: 3500,
+    },
+    {
+      numeroPedido: 3,
+      valor: 1500,
+    },
+    {
+      numeroPedido: 4,
+      valor: 5000,
+    },
+    {
+      numeroPedido: 5,
+      valor: 2750,
+    },
+    {
+      numeroPedido: 6,
+      valor: 4200,
+    },
+    {
+      numeroPedido: 7,
+      valor: 3200,
+    },
+    {
+      numeroPedido: 8,
+      valor: 1800,
+    },
+    {
+      numeroPedido: 9,
+      valor: 2500,
+    },
+    {
+      numeroPedido: 10,
+      valor: 1000,
+    },
+  ];
+  res.render("pedidos", {
+    pedidos: pedidos,
+  });
+});
 export default router;
