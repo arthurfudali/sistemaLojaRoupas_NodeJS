22383eb9229e7f22a00207a59fe23ca67dd0033f
Arthur Fudali <arthurfudali5@gmail.com>
2024-10-31 20:41:04 -0300

    cleanup: remoção de texto desnecessário comentado

--- controllers/PedidosController.js
+++ controllers/PedidosController.js
@@ -64,51 +64,5 @@ router.post("/pedidos/update",Auth, (req, res) => {
       console.log(error);
     });
 });
-/* router.get("/pedidos", (req, res) => {
-  const pedidos = [
-    {
-      numeroPedido: 1,
-      valor: 2000,
-    },
-    {
-      numeroPedido: 2,
-      valor: 3500,
-    },
-    {
-      numeroPedido: 3,
-      valor: 1500,
-    },
-    {
-      numeroPedido: 4,
-      valor: 5000,
-    },
-    {
-      numeroPedido: 5,
-      valor: 2750,
-    },
-    {
-      numeroPedido: 6,
-      valor: 4200,
-    },
-    {
-      numeroPedido: 7,
-      valor: 3200,
-    },
-    {
-      numeroPedido: 8,
-      valor: 1800,
-    },
-    {
-      numeroPedido: 9,
-      valor: 2500,
-    },
-    {
-      numeroPedido: 10,
-      valor: 1000,
-    },
-  ];
-  res.render("pedidos", {
-    pedidos: pedidos,
-  });
-}); */
+
 export default router;
